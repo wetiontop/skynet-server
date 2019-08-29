8c924164899aeae3e1caf023eb4d222ae6af0117
Wetion <wetiontop@gmail.com>
2019-08-29 16:06:09 +0800

    Update random.lua

--- src/base/random.lua
+++ src/base/random.lua
@@ -2,32 +2,25 @@ local skynet = require "skynet"
 
 local random = {}
 
-local random_type = ""--"linux_urandom"
 local random_seed = os.time() + skynet.self()
 
 function random.rand(i, j)
     random_seed = random_seed + 1
-    if random_type == "linux_urandom" then --随机性中 快
-        local data = io.open("/dev/urandom", "r"):read(4)
-        math.randomseed(os.time() + data:byte(1) + (data:byte(2) * 256) + (data:byte(3) * 65536) + (data:byte(4) * 4294967296))
-        return math.random(i, j)
-    else
-        math.randomseed(random_seed)
-        return math.random(i, j)
-    end
+    math.randomseed(random_seed)
+    return math.random(i, j)
 end
 
-function random.random_one(lst)
-    return lst[random.rand(1, #lst)]
+function random.one(list)
+    return list[random.rand(1, #list)]
 end
 
-function random.random_shuffle(a)
-    local c = #a
-    for i=1, c do
-        local ndx0 = random.rand(1, c)
-        a[ndx0], a[i] = a[i], a[ndx0]
+function random.shuffle(list)
+    local len = #list
+    for i=1, len do
+        local ndx0 = random.rand(1, len)
+        list[ndx0], list[i] = list[i], list[ndx0]
     end
-    return a
+    return list
 end
 
 return random
